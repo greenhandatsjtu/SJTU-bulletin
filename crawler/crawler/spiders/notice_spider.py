fad7209e6514bd28f69510f2d380743433563c11
sunhengke <i-hengke.sun@clobotics.com>
2021-04-18 20:36:34 +0800

    adapting new jwc website

--- crawler/crawler/spiders/notice_spider.py
+++ crawler/crawler/spiders/notice_spider.py
@@ -11,13 +11,13 @@ class NoticeSpider(scrapy.Spider):
 
     # 声明请求链接和对应的解析函数
     def start_requests(self):
-        yield scrapy.Request(url='http://jwc.sjtu.edu.cn/web/sjtu/198076.htm', callback=self.parseJwc)
-        yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/705-1-20.htm', callback=self.parseXsb)
-        yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/3016-1-20.htm', callback=self.parseXsb)
-        yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/2496-1-20.htm', callback=self.parsePartTime)
-        yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/2495-1-20.htm', callback=self.parseFullTime)
-        yield scrapy.Request(url='https://www.sjtu.edu.cn/tg/index.html', callback=self.parseSjtuNotice)
-        yield scrapy.Request(url='http://ourhome.sjtu.edu.cn/news', callback=self.parseOurHome)
+        yield scrapy.Request(url='http://www.jwc.sjtu.edu.cn/xwtg/tztg.htm', callback=self.parseNewJwc)
+        # yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/705-1-20.htm', callback=self.parseXsb)
+        # yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/3016-1-20.htm', callback=self.parseXsb)
+        # yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/2496-1-20.htm', callback=self.parsePartTime)
+        # yield scrapy.Request(url='http://xsb.seiee.sjtu.edu.cn/xsb/list/2495-1-20.htm', callback=self.parseFullTime)
+        # yield scrapy.Request(url='https://www.sjtu.edu.cn/tg/index.html', callback=self.parseSjtuNotice)
+        # yield scrapy.Request(url='http://ourhome.sjtu.edu.cn/news', callback=self.parseOurHome)
 
     # 爬取教务处通知
     def parseJwc(self, response):
@@ -30,6 +30,23 @@ class NoticeSpider(scrapy.Spider):
             item['href'] = response.urljoin(new.css('a::attr(href)').get())
             item['_type'] = "jwc"
             yield item
+
+    # 爬取新教务处通知
+    def parseNewJwc(self, response):
+        news = response.css('.Newslist li.clearfix')
+        for new in news:
+            item = NoticeItem()
+            item['title'] = new.css('.wz>a>h2::text').get()
+            item['href'] = response.urljoin(new.css('.wz>a::attr(href)').get())
+            item['_type'] = "jwc"
+            # yield item
+            yield scrapy.Request(item['href'], callback=self.getJwcNoticeDate, meta={"item": item})
+
+    # get jwc notice publish date
+    def getJwcNoticeDate(self, response):
+        item = response.request.meta['item']
+        item['date'] = response.css('.content-title>i::text').re(r'\d{4}-\d{2}-\d{2}')[0]
+        yield item
 
     # 爬取学生办通知
     def parseXsb(self, response):
@@ -70,7 +87,11 @@ class NoticeSpider(scrapy.Spider):
         for new in news:
             item = NoticeItem()
             item['title'] = new.css('a::attr(title)').extract_first()
-            item['date'] = re.sub("\.", "-", new.css('span::text').extract_first())
+            date = new.css('span::text').extract_first()
+            if date:
+                item['date'] = re.sub(r"\.", "-", date)
+            else:
+                continue
             item['href'] = response.urljoin(new.css('a::attr("href")').extract_first())
             item['_type'] = "sjtuNotice"
             yield item
